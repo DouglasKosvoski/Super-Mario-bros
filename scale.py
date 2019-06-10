f854c42dd74dffc98d60c997052fe3bc0f29ec9c
DouglasKosvoski <douglas.contactpro@gmail.com>
2019-06-10 20:41:56 -0300

    make it clear to read

--- scale.py
+++ scale.py
@@ -2,7 +2,7 @@
 
 from PIL import Image
 
-img = Image.open('tijolo_padrao.png')
+img = Image.open('image_name_default.png')
 
 new_img = img.resize((30, 30))
-new_img.save("tijolo_padrao_resized.png", "PNG", optimize=True)
+new_img.save("new_image_name.png", "PNG", optimize=True)
