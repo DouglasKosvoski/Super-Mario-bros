faaac5622927de8536493dc99cf4d5dce56d1b4c
DouglasKosvoski <douglas.contactpro@gmail.com>
2019-06-14 11:08:41 -0300

    increased readability by commenting the code

--- event.py
+++ event.py
@@ -8,7 +8,9 @@ class Event():
 
     def check_event(self, player, bkgd, hit_boxes):
         for event in pygame.event.get():
+            # check is 'close' buttom was clicked
             if event.type == pygame.QUIT:
+                # closes the game
                 pygame.quit()
                 quit()
 
@@ -19,8 +21,9 @@ class Event():
                     pygame.quit()
                     quit()
 
-                # if jey pressed is SPACE or UP ARROW makes player jump
+                # if key pressed is SPACE or UP ARROW makes player jump
                 if event.key == pygame.K_SPACE or event.key == pygame.K_UP:
+                    # since player is jumping
                     player.on_ground = False
                     player.show(0, player.rotated, jump=player.on_ground)
 
@@ -28,10 +31,8 @@ class Event():
         # key 'a' or LEFT ARROW
         if pygame.key.get_pressed()[pygame.K_a] or pygame.key.get_pressed()[pygame.K_LEFT]:
             if bkgd.x < bkgd.left_limit:
-                if player.facing_wall == False:
-                    hit_boxes.move_left(bkgd)
+                hit_boxes.move_left(bkgd)
 
-                player.last_key = 'left'
                 # moves background to right
                 bkgd.x += bkgd.speed
                 # rotate player to face left direction
@@ -41,10 +42,8 @@ class Event():
         # key 'd' or RIGHT ARROW
         elif pygame.key.get_pressed()[pygame.K_d] or pygame.key.get_pressed()[pygame.K_RIGHT]:
             if bkgd.x > bkgd.right_limit:
-                if player.facing_wall == False:
-                    hit_boxes.move_right(bkgd)
+                hit_boxes.move_right(bkgd)
 
-                player.last_key = 'right'
                 # moves background to left
                 bkgd.x -= bkgd.speed
                 # rotate player to normal position (right)
