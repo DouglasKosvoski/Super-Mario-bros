104f9d98da74f1690f2e98ad4e798fe06f815b7f
DouglasKosvoski <douglas.contactpro@gmail.com>
2019-06-13 22:05:42 -0300

    deleted scale.py since I see no use for it anymore

--- scale.py
+++
@@ -1,8 +0,0 @@
-# script used to resize images
-
-from PIL import Image
-
-img = Image.open('image_name_default.png')
-
-new_img = img.resize((30, 30))
-new_img.save("new_image_name.png", "PNG", optimize=True)
