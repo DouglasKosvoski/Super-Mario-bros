860ff302fd9b8aec8697075988ecd1e529cb0e9b
DouglasKosvoski <douglas.contactpro@gmail.com>
2019-06-23 11:48:56 -0300

    Organized and explained the code

--- display_msg.py
+++ display_msg.py
@@ -1,12 +1,28 @@
 import pygame
 
+# create area to text be drawn
 def text_objects(text, font):
-    textSurface = font.render(text, True, (130,210,180))
-    return textSurface, textSurface.get_rect()
+    color = (130,210,180)
+    # create a invisible block to the text appear
+    text_block = font.render(text, True, color)
+    # return this block and its dimensions
+    return text_block, text_block.get_rect()
 
+
+# makes the text appear in the screen
 def message_display(text, bkgd):
-    largeText = pygame.font.SysFont('linuxbiolinumo', 75)
-    TextSurf, TextRect = text_objects(text, largeText)
-    TextRect.center = ((bkgd.width/2),(bkgd.height/2.5))
-    bkgd.display.blit(TextSurf, TextRect)
+    # message position
+    x, y = (bkgd.width/2),(bkgd.height/3)
+    font_size = 75
+    font_style = 'linuxbiolinumo'
+
+
+    text_obj = pygame.font.SysFont(font_style, font_size)
+    text_surf, text_pos = text_objects(text, text_obj)
+    # centralize the message within the 'invisible box'
+    text_pos.center = (x, y)
+
+    # put this text in the screen
+    bkgd.display.blit(text_surf, text_pos)
+    # update here due to a time.freeze in the main.py
     pygame.display.update()
