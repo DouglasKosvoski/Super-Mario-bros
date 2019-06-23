344998ae8b5ce844ca3b91ab9e40f2b9ea921244
DouglasKosvoski <douglas.contactpro@gmail.com>
2019-06-23 11:48:17 -0300

    Player now returns to the ground only by gravity forces, changed animation speed, corrected hitbox issue

--- display.py
+++ display.py
@@ -1,21 +1,30 @@
 import pygame
 
 class Canvas():
-    width  = 600
-    height = 448
+    # screen dimensions (resolution)
+    width  = 900
+    height = 670
+    # create the screen
     display = pygame.display.set_mode((width, height))
 
     def __init__(self, player):
         # load image backgroud
         self.image = pygame.image.load('images/world.png')
+        # define internal clock
         self.time  = pygame.time.Clock()
-        self.fps = 60
+        # move the background in the velocity as the player
         self.speed = player.xspeed
+        # define how many updates per second
+        self.fps   = 60
 
+        # initial pos for the background (will be changed a lot)
         self.x = 0
         self.y = 0
+        # how much left can player go
         self.left_limit  = 0
-        self.right_limit = -3450
+        # how much right can the player go
+        # is negative cause the bkgd moves in the oposite direction
+        self.right_limit = -5175
 
     def show(self, player):
         # display title and fps
